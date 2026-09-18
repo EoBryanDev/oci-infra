36f8a5e6a6e12930b341931cdc5ac1ca9708a1c8
zOS-Bryan-Galaxy <eobryandev@gmail.com>
2026-09-18 18:27:07 -0300

    fix: adjusting lb manifest

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -23,10 +23,11 @@ resource "oci_load_balancer_backend_set" "http_backend_set" {
 }
 
 # Associa o Master (onde roda o Traefik/Ingress por padrão) ao Load Balancer
-resource "oci_load_balancer_backend" "http_backend" {
+resource "oci_load_balancer_backend" "http_backends" {
+  for_each         = oci_core_instance.k3s_nodes
   load_balancer_id = oci_load_balancer_load_balancer.k3s_lb.id
   backendset_name  = oci_load_balancer_backend_set.http_backend_set.name
-  ip_address       = oci_core_instance.k3s_nodes["k3s-master"].private_ip
+  ip_address       = each.value.private_ip
   port             = 80
 }
 
@@ -36,4 +37,31 @@ resource "oci_load_balancer_listener" "http_listener" {
   default_backend_set_name = oci_load_balancer_backend_set.http_backend_set.name
   port                     = 80
   protocol                 = "TCP"
+}
+
+resource "oci_load_balancer_backend_set" "https_backend_set" {
+  load_balancer_id = oci_load_balancer_load_balancer.k3s_lb.id
+  name             = "https_backend_set"
+  policy           = "ROUND_ROBIN"
+
+  health_checker {
+    protocol = "TCP"
+    port     = 443
+  }
+}
+
+resource "oci_load_balancer_listener" "https_listener" {
+  load_balancer_id         = oci_load_balancer_load_balancer.k3s_lb.id
+  name                     = "https_listener"
+  default_backend_set_name = oci_load_balancer_backend_set.https_backend_set.name
+  port                     = 443
+  protocol                 = "TCP"
+}
+
+resource "oci_load_balancer_backend" "https_backends" {
+  for_each         = oci_core_instance.k3s_nodes
+  load_balancer_id = oci_load_balancer_load_balancer.k3s_lb.id
+  backendset_name  = oci_load_balancer_backend_set.https_backend_set.name
+  ip_address       = each.value.private_ip
+  port             = 443
 }
